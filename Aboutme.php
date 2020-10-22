941af1810c5cbeae10d5666e3d0ec505b157734e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-22 18:30:59 +0200

    change to visualstudio

---
+++ Aboutme.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body style="
+background-image: url('background.jpg');
+background-repeat: no-repeat;
+background-position: center top;
+background-size: auto 800pt;">
+
+
+  <div class="title">Jonas Voigt</div>
+
+  <header>
+    <div class="header-button">Projects</div>
+    <div class="header-button">About me</div>
+    <div class="header-button">Contact</div>
+  </header>
+
+  <p style="margin: 10pt"> Welcome to my Homepage!</p>
+
+
+  <footer>
+    <p id="licence">2020 Jonas Voigt</p>
+    <img class="social-media-link" src="Icons/Mail.svg" type="image/svg" />
+    <img class="social-media-link" src="Icons/Youtube.svg" type="image/svg" />
+    <img class="social-media-link" src="Icons/Github.svg" type="image/svg" />
+    <img class="social-media-link" src="Icons/Twitter.svg" type="image/svg" />
+  </footer>
+
+</body>
+
+</html>
---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body style="
+background-image: url('background.jpg');
+background-repeat: no-repeat;
+background-position: center top;
+background-size: auto 800pt;">
+
+
+  <div class="title">Jonas Voigt</div>
+
+  <header>
+    <div class="header-button">Projects</div>
+    <div class="header-button">About me</div>
+    <div class="header-button">Contact</div>
+  </header>
+<div class="textbox">
+<p>
+  Hello my Name is Jonas Voigt,
+  I have just started to study software ingeneering at the RWTH university in Aachen, Germany.
+  For about 10 Years programming has beed one of my biggest Hobbies. Now I want to share some of my future Projects on this Website.
+  Hopefully you can get Inspired or learn something.
+  If you need help, have questions or ideas, feel free to contact me via Mail or the contact form.
+  See you, Jonas
+</p>
+</div>
+
+</html>
