832a2b052e2295cbf7d9c830b4695b05f1002b22
BuildTools <unconfigured@null.spigotmc.org>
2020-10-24 11:26:56 +0200

    updates 23Okt

--- home.php
+++ home.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
 
 </head>
 
@@ -13,28 +13,32 @@ background-repeat: no-repeat;
 background-position: center top;
 background-size: auto 800pt;">
 
+    <div class="wrapper">
+        <div class="title">Jonas Voigt</div>
 
-  <div class="title">Jonas Voigt</div>
-
-  <header>
-    <div class="header-button">Projects</div>
-    <div class="header-button">About me</div>
-    <div class="header-button">Contact</div>
-  </header>
-<div class="textbox">
-<p>
-  Hello my Name is Jonas Voigt,
-  I have just started to study software ingeneering at the RWTH university in Aachen, Germany.
-  For about 10 Years programming has beed one of my biggest Hobbies. Now I want to share some of my future Projects on this Website.
-  Hopefully you can get Inspired or learn something.
-  If you need help, have questions or ideas, feel free to contact me via Mail or the contact form.
-  Jonas
-  
-</p>
-</div>
+        <header>
+            <div class="header-button">Projects</div>
+            <div class="header-button">About me</div>
+            <div class="header-button">Contact</div>
+        </header>
 
 
-  <?php include "footer.html" ?>
+        <div class="textbox">
+            <p>
+                Hello my Name is Jonas Voigt,
+                I have just started to study software ingeneering at the RWTH university in Aachen, Germany.
+                For about 10 Years programming has been one of my biggest Hobbies. Now I want to share some of my future
+                Projects on this Website.
+                Hopefully you can get Inspired or learn something.
+                If you need help, have questions or ideas, feel free to contact me via Mail or the contact form.
+                Jonas
+
+            </p>
+
+
+        </div>
+    </div>
+    <?php include "footer.html" ?>
 
 </body>
 
