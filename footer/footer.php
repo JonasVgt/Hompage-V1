72e6ea0c2c83b3cd1a947f1054c8e41a190ce416
BuildTools <unconfigured@null.spigotmc.org>
2020-11-03 19:07:08 +0100

    Updated footer

--- footer/footer.php
+++ footer/footer.php
@@ -3,8 +3,6 @@
 <link rel="stylesheet" type="text/css" href="footer/footer.css">
 
 <footer>
-    <p class="footer-part" id="licence">2020 Jonas Voigt</p>
-
     <div class="footer-part">
         <a href="mailto: jonas17voigt@gmail.com" target="_blank">
             <img class="social-media-link" src="Icons/Mail.svg" type="image/svg" /></a>
@@ -18,12 +16,15 @@
         <a href="https://twitter.com" target="_blank">
             <img class="social-media-link" src="Icons/Twitter.svg" type="image/svg" /></a>
     </div>
-    <div style="text-align: end" class="footer-part" >
-        <a div class="footer-link" href="privacy.php">PRIVACY</a>
-        <a div class="footer-link" href="privacy.php">PRIVACY</a>
-        <a div class="footer-link" href="privacy.php">PRIVACY</a>
-        <a div class="footer-link" href="privacy.php">PRIVACY</a>
-        <a div class="footer-link" href="privacy.php">PRIVACY</a>
+    <div  class="footer-part">
+        <a div class="hidden-link" href="privacy.php">PROJECTS</a> |
+        <a div class="hidden-link" href="privacy.php">ABOUT ME</a> |
+        <a div class="hidden-link" href="privacy.php">CONTACT</a>
+
     </div>
+    <p id="copyright"><a class="hidden-link" href="index.php">&copy; 2020: Jonas Voigt</a></p>
+
+
+
 
 </footer>
