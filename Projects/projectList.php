83369dcfaa70df197de226cb839d4982fa26bd89
JonasVgt <55986264+JonasVgt@users.noreply.github.com>
2020-12-20 17:28:52 +0100

    added Project list

---
+++ Projects/projectList.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="/style.css">
+    <link rel="stylesheet" type="text/css" href="/Projects/articleList.css">
+</head>
+
+<body>
+
+    <div class="wrapper">
+
+        
+        <div style="background-color:gray">
+        <?php
+        include("../header/header.php");        
+        ?>
+        </div>
+
+
+
+        <div class="article">
+            <img class="article-thumbnail" src="/article1.jpg">
+            <p class="title">Artikel 1!! Toll macht spass!</p>
+            <p class="text-preview">Das hier ist der Text. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci minus repudiandae mollitia non beatae quae iste eum ab error fugit eius, neque nisi possimus veniam, sunt velit soluta aspernatur distinctio....</p>
+            <p class="date">19.12.2020</p>
+        </div>
+
+
+
+
+    <?php include "../footer/footer.php" ?>
+
+</body>
+
+</html>
