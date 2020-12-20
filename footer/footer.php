853a35b8e6177ddfd0c81371b0a03219cffa31b7
JonasVgt <55986264+JonasVgt@users.noreply.github.com>
2020-12-20 17:28:36 +0100

    changed filepaths

--- footer/footer.php
+++ footer/footer.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 
-<link rel="stylesheet" type="text/css" href="footer/footer.css">
+<link rel="stylesheet" type="text/css" href="/footer/footer.css">
 
 <footer>
+    
     <div class="footer-part">
         <a href="mailto: jonas17voigt@gmail.com" target="_blank">
-            <img class="social-media-link" src="Icons/Mail.svg" type="image/svg" /></a>
+            <img class="social-media-link" src="/Icons/Mail.svg" type="image/svg" /></a>
 
         <a href="https://youtube.com" target="_blank">
-            <img class="social-media-link" src="Icons/Youtube.svg" type="image/svg" /></a>
+            <img class="social-media-link" src="/Icons/Youtube.svg" type="image/svg" /></a>
 
         <a href="https://github.com" target="_blank">
-            <img class="social-media-link" src="Icons/Github.svg" type="image/svg" /></a>
+            <img class="social-media-link" src="/Icons/Github.svg" type="image/svg" /></a>
 
         <a href="https://twitter.com" target="_blank">
-            <img class="social-media-link" src="Icons/Twitter.svg" type="image/svg" /></a>
+            <img class="social-media-link" src="/Icons/Twitter.svg" type="image/svg" /></a>
     </div>
     <div  class="footer-part">
-        <a div class="hidden-link" href="privacy.php">PROJECTS</a> |
-        <a div class="hidden-link" href="privacy.php">ABOUT ME</a> |
-        <a div class="hidden-link" href="privacy.php">CONTACT</a>
+        <a div class="hidden-link" href="/Projects/projectList.php">PROJECTS</a> |
+        <a div class="hidden-link" href="/privacy.php">ABOUT ME</a> |
+        <a div class="hidden-link" href="/privacy.php">CONTACT</a>
 
     </div>
-    <p id="copyright"><a class="hidden-link" href="index.php">&copy; 2020: Jonas Voigt</a></p>
+    <p id="copyright"><a class="hidden-link" href="/index.php">&copy; 2020: Jonas Voigt</a></p>
 
 
 
--- header/header.php
+++ header/header.php
@@ -1,14 +1,15 @@
-<link rel="stylesheet" type="text/css" href="header/header.css">
+<link rel="stylesheet" type="text/css" href="/header/header.css">
 
-<a href="index.php"><div class="title">Jonas Voigt</div></a>
+<a href="/index.php"><div class="title">Jonas Voigt</div></a>
 
 
 <header>
     <ul>
         
-    <li><div class="header-button">Projects</div></li>
+    <li><a href="/Projects/projectList.php">
+        <div class="header-button">Projects</a></div></li>
     <li><div class="header-button">About me</div></li>
-    <li><a href="contact.php">
+    <li><a href="/contact.php">
         <div class="header-button">Contact</div>
     </a></li>
 </ul>
